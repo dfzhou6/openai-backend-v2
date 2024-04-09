ca49b68e72d4aaf59cfcd2333a98e9a7cb3a4111
felix.zhou <felix.zhou@shopee.com>
2024-04-10 00:28:04 +0800

    添加token以及单点登录功能

--- app/Biz/LoginBiz.php
+++ app/Biz/LoginBiz.php
@@ -7,16 +7,27 @@ use App\Models\User;
 use App\Utils\CommonUtils;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 
 class LoginBiz
 {
     public static function Check(Request $request)
     {
         $username = $request->get('username');
-        $key = sprintf(config('constants.USER_LOGIN_KEY'), $username);
-
-        if (empty($username) || !Cache::has($key)) {
+        $token = $request->get('token');
+        if (empty($username)) {
             return CommonUtils::RspError('请重新登录');
+        }
+
+        $user = User::firstWhere('username', $username);
+        if (empty($user)) {
+            return CommonUtils::RspError('请重新登录');
+        }
+        if ($user->token !== $token) {
+            return CommonUtils::RspError('已在其他地方登录，请重新登录');
+        }
+        if ($user->token_expire_time < time()) {
+            return CommonUtils::RspError('登录已失效，请重新登录');
         }
 
         return CommonUtils::RspSuccess('');
@@ -31,17 +42,20 @@ class LoginBiz
             return CommonUtils::RspError('user username or password incorrect');
         }
 
+        // fe: bcrypt.hashSync(md5('si1ow4ox_' + this.password), 10)
         if (!password_verify($user->password, $password)) {
             return CommonUtils::RspError('user username or password incorrect');
         }
 
-        $key = sprintf(config('constants.USER_LOGIN_KEY'), $username);
-        Cache::put($key, $user->toJson(), config('constants.USER_LOGIN_EXPIRED'));
+        $user->token = Str::uuid()->toString();
+        $user->last_login_time = time();
+        $user->token_expire_time = $user->last_login_time + config('constants.USER_LOGIN_EXPIRED');
 
         return CommonUtils::RspSuccess([
-            'username' => $username,
-            'last_login_time' => date('Y-m-d H:i:s', $user['last_login_time']),
-            'expire_time' => time() + config('constants.USER_LOGIN_EXPIRED')
+            'username' => $user->username,
+            'last_login_time' => date('Y-m-d H:i:s', $user->last_login_time),
+            'token_expire_time' => $user->token_expire_time,
+            'token' => $user->token,
         ]);
     }
 }
